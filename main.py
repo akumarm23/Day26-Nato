aecb9e77784c2b006743166e0814b3dc55f9894a
akumarm23 <ak.muniswamy2023@gmail.com>
2023-12-13 19:14:25 +0530

    Updated on Day30; Includes Error Handling

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ import pandas
 # Load the NATO phonetic alphabet data from the CSV file
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# TODO 1: Create a dictionary mapping letters to their corresponding phonetic code
 # Using a dictionary comprehension to iterate over rows in the DataFrame and create the mapping
 ph_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(ph_dict)  # Uncomment to check the created dictionary
 
-# TODO 2: Create a list of the phonetic code words for a word entered by the user
+def generate_phonetic():
+    # Get user input for a word and convert it to uppercase
+    word = input("Enter a word: ").upper()
+    try:
+        # Create a list of phonetic code words using the dictionary created earlier
+        output = [ph_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters from A-Z please.\n")
+        generate_phonetic()
+    else:
+        print(output)
 
-# Get user input for a word and convert it to uppercase
-word = input("Enter a word: ").upper()
-# Create a list of phonetic code words using the dictionary created earlier
-output = [ph_dict[letter] for letter in word]
-print(output)
+generate_phonetic()
+
 
 # END OF CODE
